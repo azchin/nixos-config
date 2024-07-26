c0b4d1bce35f3cc5c1821b82c5ad46eb4ed78575
Andrew Chin <correctbutnotright@outlook.com>
2024-07-26 00:39:29 -0400

    rebuild commands

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 set -eu
+dir=$(dirname $(realpath $0))
+cmd="${1:-switch}"
+cd $dir
 git add private
-sudo nixos-rebuild switch --flake .
+case $cmd in
+    boot)
+        sudo nixos-rebuild boot --flake . || :
+        ;;
+    upgrade)
+        sudo nixos-rebuild switch --upgrade --flake . || :
+        ;;
+    switch)
+        sudo nixos-rebuild switch --flake . || :
+        ;;
+esac
 git restore --staged private
