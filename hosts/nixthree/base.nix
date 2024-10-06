8d6764de7e6fb8da3b11fad712d5c013ca082aaa
Andrew Chin <correctbutnotright@outlook.com>
2024-10-06 15:53:15 -0400

    prepare for nixthree

---
+++ hosts/nixthree/base.nix
@@ -0,0 +1,54 @@
+{ config, lib, pkgs, pkgs-stable, ... }:
+
+{
+  imports = [
+    ../../configuration.nix
+  ];
+
+  # Define your hostname.
+  networking.hostName = "nixthree";
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Configure custom modules
+  nixoneNetwork.enable = true;
+  myDesktop.enable = true;
+  myHypr.enable = true;
+  myX11.dpi = 96;
+  myCursorSize = 36;
+
+  # TODO clean this up into multihead target or module
+  services.xserver.xrandrHeads = [ 
+    {
+      output = "DP-2";
+      # DisplaySize 338 190\n
+      monitorConfig = ''
+        Option "LeftOf DP-1
+      '';
+    }
+    {
+      output = "DP-1";
+      primary = true;
+    }
+  ];
+
+  myGaming.enable = true;
+  
+  # Hardware configuration
+  # nixthreeHardware.enable = true;
+  # nixthreeHardwareExtra.enable = true;
+
+  # Bootloader
+  myEFI.enable = true;
+
+  # Graphics
+  myAmdgpu.enable = true;
+  
+  # Install programs specific for this host
+  environment.systemPackages = with pkgs; [
+    btrfs-progs
+  ];
+
+  # system.stateVersion = "24.05"; # Did you read the comment?
+}
