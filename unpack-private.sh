335579f0baf067755a7c5e0960c2c6998256fcc4
Andrew Chin <correctbutnotright@outlook.com>
2024-11-16 15:07:04 -0500

    automate gnupg setup

--- unpack-private.sh
+++ unpack-private.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -eu
+if ! [ -f $HOME/.gnupg/gpg-agent.conf ]; then
+    mkdir -p $HOME/.gnupg
+    touch $HOME/.gnupg/gpg-agent.conf
+fi
+if ! [ -d $HOME/.local/share/gnupg ]; then
+    mkdir -p $HOME/.local/share/gnupg
+    chmod 700 $HOME/.local/share/gnupg
+fi
 rm -r private-old || :
 cp -r private private-old || :
 gpg -d private.tar.gz.gpg > private.tar.gz
--- update-private.sh
+++ update-private.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 set -eu
+if ! [ -f $HOME/.gnupg/gpg-agent.conf ]; then
+    mkdir -p $HOME/.gnupg
+    touch $HOME/.gnupg/gpg-agent.conf
+fi
+if ! [ -d $HOME/.local/share/gnupg ]; then
+    mkdir -p $HOME/.local/share/gnupg
+    chmod 700 $HOME/.local/share/gnupg
+fi
 git restore --staged private || :
 tar cvf private.tar.gz ./private
 rm private.tar.gz.gpg || :
