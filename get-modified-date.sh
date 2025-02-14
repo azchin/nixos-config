ad08ecd7c1f006fb90ddf5cca59d758d2982e81e
Andrew Chin <achin34@gatech.edu>
2025-02-14 01:17:54 -0500

    nice script to check last update

---
+++ get-modified-date.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+node="nixpkgs"
+unix_time=$(yq ".nodes.$node.locked.lastModified" flake.lock)
+today=$(date +%s)
+time_diff=$(($today - $unix_time))
+days_diff=$(echo "$time_diff / 86400" | bc)
+echo -n "$node last modified: "
+date -d @"$unix_time"
+# echo -n "Today: "
+# date
+echo "Last updated $days_diff days ago"
