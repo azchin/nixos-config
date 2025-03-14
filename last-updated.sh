147f7c4a08d6454fa5e0544bd87bb84f2b26c87c
Andrew Chin <achin34@gatech.edu>
2025-03-13 21:00:23 -0400

    updated script

--- last-updated.sh
+++ last-updated.sh
@@ -4,8 +4,6 @@ unix_time=$(yq ".nodes.$node.locked.lastModified" flake.lock)
 today=$(date +%s)
 time_diff=$(($today - $unix_time))
 days_diff=$(echo "$time_diff / 86400" | bc)
-echo -n "$node last modified: "
+echo -n "$node last modified "
 date -d @"$unix_time"
-# echo -n "Today: "
-# date
 echo "Last updated $days_diff days ago"
