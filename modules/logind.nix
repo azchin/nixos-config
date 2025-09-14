960598d7f0ea9a4b7fa9a2661f2fe50817cbfef5
Andrew Chin <achin34@gatech.edu>
2025-09-14 12:33:11 -0400

    update logind setting names

--- modules/logind.nix
+++ modules/logind.nix
@@ -6,10 +6,10 @@
   };
   
   config = lib.mkIf config.myLogind.enable {
-    services.logind = {
-      powerKey = "suspend-then-hibernate";
-      powerKeyLongPress = "poweroff";
-      lidSwitch = "suspend-then-hibernate";
+    services.logind.settings.Login = {
+      HandlePowerKey = "suspend-then-hibernate";
+      HandlePowerKeyLongPress = "poweroff";
+      HandleLidSwitch = "suspend-then-hibernate";
     };
     systemd.sleep.extraConfig = "HibernateDelaySec=20m";
   };
